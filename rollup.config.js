2b8ee7bbe87115a8c5df711e96ba5914fb508435
royfuwei <royfuwei@gmail.com>
2025-06-24 14:58:54 +0800

    build(rollup): adjust rollup config external dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,23 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { rollupDevNodemonPlugin } from './scripts/devNodemonPlugin.mjs';
 import { rollupExternalWatch } from './scripts/rollupExternalWatch.mjs';
+import fs from 'fs';
 // import terser from '@rollup/plugin-terser';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
+// 讀取 root package.json，標記 external
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const externalDeps = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
 export default {
   input: 'src/main.ts',
+  external: externalDeps,
   plugins: [
     rollupExternalWatch(),
     json(),
