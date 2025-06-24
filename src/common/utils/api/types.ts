2a5601fb962502025678e2809c21adb05eafe715
royfuwei <royfuwei@gmail.com>
2025-06-24 12:20:33 +0800

    feat(utils): add api utils tool

---
+++ src/common/utils/api/types.ts
@@ -0,0 +1,39 @@
+export interface TDataType<T> {
+  new (...args: any[]): T;
+}
+
+export type ApiResStatus = {
+  success: boolean;
+  status: number;
+};
+
+export type ApiResData<T = any> = ApiResStatus & {
+  data: T | T[] | null;
+};
+
+export type ApiResPaginated<T = any> = ApiResStatus & {
+  total: number;
+  data: T[];
+};
+
+export type ApiResError = ApiResStatus & {
+  errorCode: number;
+  message: string;
+  description?: string;
+  path?: string;
+  timestamp?: Date;
+  method?: string;
+};
+
+export type ApiResPaginatedParams<TData> = {
+  success: boolean;
+  status: number;
+  total: number;
+  data: TData[];
+};
+
+export type ApiResDataParams<TData> = {
+  success: boolean;
+  status: number;
+  data: TData | TData[] | null;
+};
