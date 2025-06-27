48132621f5efb2b1e37e1452e8f26292e4353eb4
royfuwei <royfuwei@gmail.com>
2025-06-27 15:17:02 +0800

    feat(common): add common middlewares and import to routing controller

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { RoutingControllersOptions, useContainer } from 'routing-controllers';
 import { initKoaApp } from '@/koaApp';
 import { iocAdapter } from '@/ioc/iocAdapter';
 import * as _indexControllers from '@/delivery/controllers';
+import * as _indexMiddlewares from '@/common/middlewares';
 import { container } from 'tsyringe';
 import { IocRegistryApp } from '@/ioc/ioc.register.app';
 
@@ -11,12 +12,14 @@ export function server() {
   useContainer(iocAdapter);
 
   const controllers = Object.values(_indexControllers).values();
+  const middlewares = Object.values(_indexMiddlewares).values();
 
   const routingControllerOptions: RoutingControllersOptions = {
     defaultErrorHandler: false,
     classTransformer: true,
     validation: true,
     controllers: [...controllers],
+    middlewares: [...middlewares],
   };
 
   const app = initKoaApp(routingControllerOptions);
